c10f662313e4c28b9f640e9bd1434ae04f323b60
Finn Mefford <finnmefford@gmail.com>
2021-07-14 21:19:49 -0400

    Tiles can detect if there are adjacent tiles

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.Tilemaps;
 public class MapManager : MonoBehaviour
 {
     private Tilemap map;
+    public Tile currTile;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,23 +23,26 @@ public class MapManager : MonoBehaviour
     /// This function updates the road at a given position on the tilemap. It will ensure that the tile at the position and all surronding positions appear continuous. 
     /// </summary>
     /// <param name="pos"> The position at which to update the road. </param>
-    public void updateRoad(Vector3Int pos)
+    public void UpdateRoad(Vector3Int pos)
     {
         bool[] adjacentTiles = { false, false, false, false };
         for (int i = -1; i < 2; i += 2)
         {
-            if (map.HasTile(new Vector3Int(pos.x, pos.y += i, pos.z)))
+            if (map.HasTile(new Vector3Int(pos.x, pos.y - i, pos.z)))
             {
                 adjacentTiles[i + 1] = true;
             }
-            if (map.HasTile(new Vector3Int(pos.x += i, pos.y, pos.z)))
+            if (map.HasTile(new Vector3Int(pos.x - i, pos.y, pos.z)))
             {
                 adjacentTiles[i + 2] = true;
             }
         }
         if (map.HasTile(pos))
         {
-
+            if (adjacentTiles[0] == true && adjacentTiles[1] == true)
+            {
+                Debug.Log("trigger");
+            }
         }
     }
 }
